20007bc6ac90acab5e186f96b0886ffc9e64c2e7
CreeperGuy72192 <65190117+CreeperGuy72192@users.noreply.github.com>
2024-03-10 08:01:46 -0400

    Oops

--- src/boot.asm
+++ src/boot.asm
@@ -1,121 +1,25 @@
-[org 0x7c00]
-[bits 16]
+; From: https://github.com/MrEmpy/SimpleASMKernel
 
+%INCLUDE "src/disk/memory.asm"
+[BITS 16]
+[ORG BOOT]
 
-; Skips the includes
-jmp Start
+call LoadSystem
+jmp KERNELADDR
 
-%include "src/Bootloader/Print.asm"
-%include "src/Bootloader/Disk.asm"
-%include "src/Bootloader/Memory.asm"
+%INCLUDE "src/disk/disk.asm"
 
-
-; Some BIOSes jump to the boot sector with 0x07c0:0x0000 or 0x0000:0x7c00 and other ways so we set CS to 0
-Start:
-    cli
-    jmp 0x0000:Main
-    nop
-
-
-Main:
-    ; Saves the number of the drive where we currently are
-    mov [BootDisk], dl
-
-    ; Segments setup
-    xor ax, ax
-    mov ds, ax
-    mov es, ax
-    mov fs, ax
-    mov gs, ax
-
-    ; Stack setup
-    mov ss, ax
-    mov sp, 0x7c00
-
-    sti
-
-    call GetMemoryAvaiable
-    call ReadDisk
-
-    ; Clears the screen
-    mov ah, 0
-    mov al, 3
-    int 0x10
-
-    call PrintLogo
-
-    ; Waits 4 seconds
-    mov cx, 0x2d
-    mov dx, 0x4240
-    mov ah, 0x86
-    int 0x15
-
-    ; Jumps to kernel
-    jmp 0x100:0x0
-
-
-
-; *TRASH CODE WARNING! CONTINUE AT YOUR OWN RISK
-PrintLogo:
-    xor cx, cx
-
-    .Loop:
-        cmp cx, byte 6
-        je .Logo
-
-        call PrintNewLine
-
-        inc cx
-        jmp .Loop
-
-    .Logo:
-        ; You didn't listen to the warning, ah?
-        mov si, MascLogoSpace
-        call PrintString
-
-        mov si, MascLogo
-        call PrintString
-
-        mov si, MascLogoSpace
-        call PrintString
-
-        mov si, MascLogo1
-        call PrintString
-
-        mov si, MascLogoSpace
-        call PrintString
-
-        mov si, MascLogo2
-        call PrintString
-
-        mov si, MascLogoSpace
-        call PrintString
-
-        mov si, MascLogo3
-        call PrintString
-
-        ; Welcome message
-        mov si, WelcomeSpace
-        call PrintString
-
-        mov si, WelcomeMessage
-        call PrintString
+LoadSystem:
+    mov byte[sector], 2
+    mov byte[drive], 80h
+    mov byte[sectornum], 2
+    mov word[segmentaddr], KERNELSEG ; kernel seg
+    mov word[segmentoffset], KERNELOFFSET ; kernel offset
+    call DiskRead
 
     ret
 
 
-
-
-; Don't kill me pls
-MascLogoSpace: db "                 ", 0
-MascLogo: db "  \  |                      _ \   ___|", 10, 13, 0
-MascLogo1: db " |\/ |   _` |   __|   __|  |   |\___ \", 10, 13, 0
-MascLogo2: db " |   |  (   | \__ \  (     |   |      |", 10, 13, 0
-MascLogo3: db "_|  _| \__._| ____/ \___| \___/ _____/", 10, 13, 10, 13, 0
-WelcomeSpace: db "                      ", 0
-WelcomeMessage: db "Welcome to MascOS! Loading...", 0
-
-
-; Fills the rest of the sector with 0s and boot signature
-times 510-($-$$) db 0
+jmp $
+times 510 - ($ - $$) db 0
 dw 0xaa55
--- src/kernel.asm
+++ src/kernel.asm
@@ -1,30 +1,72 @@
-[org 0x1000]
-[bits 16]
+%INCLUDE "src/disk/memory.asm"
+[BITS 16]
+[ORG KERNELOFFSET]
+
+section .bss
+    buffer resb 255    ; Define a buffer to store input (maximum size 255 bytes)
+    buffer_len resb 1  ; Variable to store the length of input
+
+section .text
 
 jmp Main
 
-; This will work for now
-%include "src/Bootloader/Print.asm"
-; %include "./Kernel/VGA.asm"
-%include "src/Kernel/Shell.asm"
-
+; Include external code
+%INCLUDE "src/utils/print.asm"
+%INCLUDE "src/utils/command.asm"
 
 Main:
-    ; Clears the screen
-    mov ah, 0
-    mov al, 3
-    int 0x10
+    call Segmen
+    call Stack
 
-    ; call VgaInit
-    ; mov al, 67
-    ; mov ah, 0
-    ; call VgaPrintChar
+    call cls
     
-    jmp InitShell
+    mov si, welcome_sys
+    call print
 
+    mov si, welcome_sys2
+    call println
+
+    mov si, sys_ver
+    call print
+
+    mov si, info1
+    call println
+
+    call newln
+
+    mov si, prompt_symb
+    call println
+
+    ; Begin typing loop
+    call command
+
+    mov si, haltedmsg
+    call println
+
+    jmp .hang
+
+.hang:
     cli
     hlt
+    
+    jmp $
 
+Stack:
+    mov ax, 0x0200b
+    mov ss, ax
+    mov sp, 0x0300b
+ret
 
-; Fills 6 sectors
-times 3072 db 0
+Segmen:
+    mov ax, es
+    mov ds, ax
+ret
+
+welcome_sys db 'Welcome to System!', 0
+welcome_sys2 db "System version ", 0
+sys_ver db "0.0.3", 0
+
+info1 db "Type 'help' for a list of commands.", 0
+
+prompt_symb db "system# > ", 0
+haltedmsg db 'System has halted!', 0
